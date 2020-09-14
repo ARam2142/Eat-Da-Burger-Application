81c8f480c01f057e07574cd87eaf15b1db8a182c
Aram2142 <andresaaramirez@aol.com>
2020-09-14 17:43:43 -0400

    made connection

---
+++ config/connection.js
@@ -0,0 +1,16 @@
+const mysql = require("mysql");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "rootroot",
+  database: "burgers_db"
+});
+
+connection.connect(function (err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId + "\n");
+});
+
+module.exports = connection;
---
+++ config/orm.js
@@ -0,0 +1,10 @@
+var connection = require("./connection.js");
+
+selectAll()
+insertOne()
+updateOne()
+
+deleteOne()
+
+
+module.exports = orm;
