5d049302fd2b1ca51047cceb4918df792e177db1
Aram2142 <andresaaramirez@aol.com>
2020-09-15 17:30:54 -0400

    made orm methods

--- config/orm.js
+++ config/orm.js
@@ -1,10 +1,47 @@
-var connection = require("./connection.js");
+var connection = require("../config/connection.js");
 
-selectAll()
-insertOne()
-updateOne()
 
-deleteOne()
+var orm = {
+    selectAll: function (table, tableInput, selection) {
+        var queryString = "SELECT FROM" + table;
+        connection.query(queryString, [tableInput, selection], function(err, res) {
+            if (err) throw err;
+            console.log(res);
+        });
+    },
+
+    //insert into sql query table
+    insertOne: function (table, columns, values) {
+        var queryString = "INSERT INTO " + table;
+        connection.query(queryString, [columns, values], function(err, res) {
+            if (err) throw err;
+            console.log(res);
+        });
+
+    },
+
+    updateOne: function (table, columns, values, condition) {
+        var queryString = "UPDATE" + table;
+        queryString += "SET";
+        queryString += "??";
+        queryString += "WHERE";
+        queryString += "??";
+
+        connection.query(queryString, [columns, values, condition], function(err, res) {
+            if (err) throw err;
+            console.log(res);
+        })
+    },
+
+    deleteOne: function (values, condition, columns) {
+        var queryString = "DELETE FROM" + table + "WHERE" + condition;
+        connection.query(queryString, [values, condition, columns], function(err, res) {
+            if (err) throw err;
+            console.log(res);
+        })
+    }
+
+}
 
 
 module.exports = orm;
