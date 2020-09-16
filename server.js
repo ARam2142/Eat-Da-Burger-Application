3ba14d4a141c5753a89b490e96112d9bead62b10
Aram2142 <andresaaramirez@aol.com>
2020-09-16 00:04:21 -0400

    another change

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 var express = require("express");
-
-var PORT = process.env.PORT || 8080;
-
+var PORT = process.env.PORT || 3000;
 var app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
