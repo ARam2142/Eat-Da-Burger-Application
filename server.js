fba20de3582d85a9b1a0a59c8fa580d0b4af2a36
Aram2142 <andresaaramirez@aol.com>
2020-09-16 18:05:28 -0400

    made server

--- server.js
+++ server.js
@@ -1,23 +1,18 @@
 var express = require("express");
 var PORT = process.env.PORT || 3000;
 var app = express();
-
-// Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static("public"));
+var exphbs = require("express-handlebars");
 
 // Parse application body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Set Handlebars.
-var exphbs = require("express-handlebars");
-
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.engine("handlebars", exphbs({ defaultLayout: "main", extname: "handlebars" }));
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
 var routes = require("./controllers/burgers_Controller.js");
-
 app.use(routes);
 
 // Start our server so that it can begin listening to client requests.
