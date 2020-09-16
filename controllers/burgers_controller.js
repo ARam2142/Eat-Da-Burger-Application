741eb71b24d255381ee0a2c7e5152c1cc886a82b
Aram2142 <andresaaramirez@aol.com>
2020-09-15 21:13:17 -0400

    router get and post up i think?

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,9 +1,28 @@
 //Dependencies
-var express = require("express");
 var burger = require("../models/burger.js");
-var router = express.Router();
+var express = require("express");
+var router = express.Router()
 
-//get the router
+//get all the routers for the app
+router.get('/', function(req, res) {
+    burger.selectAll(function(data) {
+        let burObject = {
+            burger: data
+        };
+        console.log(burObject);
+        res.render("index", burObject);
+    });
+});
+
+router.post("/api/burgers", function(req, res) {
+    burger.insertOne(["burger_name", "devoured"], 
+    [req.body.burger_name, req.body.devoured],
+    function(res) {
+        res.json({id: res.insertId});
+    });
+});
+
+router.put("/api/burgers:id")
 
 
 
