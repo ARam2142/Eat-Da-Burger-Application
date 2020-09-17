2679ba0402f313b44155915bf9841e81893854f6
Aram2142 <andresaaramirez@aol.com>
2020-09-17 00:10:34 -0400

    added to files

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,29 +1,80 @@
 //Dependencies
 var burger = require("../models/burger.js");
 var express = require("express");
-var router = express.Router()
+var router = express.Router();
 
-//get all the routers for the app
-router.get('/', function(req, res) {
-    burger.selectAll(function(data) {
-        let burObject = {
-            burger: data
+try {
+    //get all the routers for the app
+    router.get("/", function(req, res) {
+        burger.all = (data) => {
+            let hbeObject = {
+                burger: data
+            };
+            console.log(hbeObject);
+            res.render("index", hbeObject);
         };
-        console.log(burObject);
-        res.render("index", burObject);
     });
-});
 
-router.post("/api/burgers", function(req, res) {
-    burger.insertOne(["burger_name", "devoured"], 
-    [req.body.burger_name, req.body.devoured],
-    function(res) {
-        res.json({id: res.insertId});
+} catch(err) {
+    if (err) throw err;
+    console.log(err);
+}
+
+try {
+    router.post("/api/burgers", function(req, res) {
+        burger.create(["burger_name", "devoured"], 
+        [req.body.burger_name, req.body.devoured],
+        function(result) {
+            res.json({id: result.insertid});
+        });
     });
-});
 
-router.put("/api/burgers:id")
+} catch(err) {
+    if (err) throw err
+    console.log("there is an err")
+}
+
+try {
+    router.put("/api/burgers:id", function(req, res) {
+        var condition = "id = " + req.params.id;
+    
+        burger.update({devoured: req.body.devoured
+        }, condition, function(result) {
+            if(result.changedRows === 0) {
+    
+                return res.status(404).end();
+                
+            } else {
+                res.status(200).end();
+            }
+        });
+    });
+
+} catch(err) {
+    if (err) throw err;
+    console.log(err)
+}
+
+try {
+    router.delete("/api/burgers:id", function(req, res) {
+        var condition = "id = " + req.params.id;
+    
+        burger.delete({devoured: req.body.devoured
+        }, condition, function(result) {
+            if(result.changedRows === 0) {
+                return res.status(404).end();
+            } else {
+                res.status(200).end();
+            }
+        });
+    });
+
+} catch(err) {
+    if (err) throw err;
+    console.log(err);
+}
 
 
 
+//export routers to server.js
 module.exports = router;
--- models/burger.js
+++ models/burger.js
@@ -2,24 +2,24 @@
 var orm = require("../config/orm.js");
 
 var burger = {
-    all: function(cb) {
-      orm.all("burgers", function(res) {
+    selectAll: function(cb) {
+      orm.all("burger", function(res) {
         cb(res);
       });
     },
     // The variables cols and vals are arrays.
-    create: function(cols, vals, cb) {
-      orm.create("burgers", cols, vals, function(res) {
+    insertOne: function(columns, values, cb) {
+      orm.create("burger", columns, values, function(res) {
         cb(res);
       });
     },
-    update: function(objColVals, condition, cb) {
-      orm.update("burgers", objColVals, condition, function(res) {
+    updateOne: function(objColVals, condition, cb) {
+      burger.update("burger", objColVals, condition, function(res) {
         cb(res);
       });
     },
-    delete: function(condition, cb) {
-      orm.delete("burgers", condition, function(res) {
+    deleteOne: function(condition, cb) {
+      orm.delete("burger", condition, function(res) {
         cb(res);
       });
     }
