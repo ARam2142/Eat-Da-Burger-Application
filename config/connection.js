9fecaa49d1ee51a10892c6e20a9a68d6757d27cc
Aram2142 <andresaaramirez@aol.com>
2020-09-21 14:29:36 -0400

    make it better

--- config/connection.js
+++ config/connection.js
@@ -8,6 +8,14 @@ var connection = mysql.createConnection({
   database: "burgers_db"
 });
 
+var del = connection._protocol._delegateError;
+connection._protocol._delegateError = function(err, sequence){
+  if (err.fatal) {
+    console.trace('fatal error: ' + err.message);
+  }
+  return del.call(this, err, sequence);
+};
+
 connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
