e85d3c21a5220d9b591a0fbeaba5f39baa819af9
Aram2142 <andresaaramirez@aol.com>
2020-09-21 14:14:06 -0400

    make it better gain

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@ const mysql = require("mysql");
 
 var connection = mysql.createConnection({
   host: "localhost",
-  port: 3306,
+  port: "3306",
   user: "root",
   password: "rootroot",
   database: "burgers_db"
