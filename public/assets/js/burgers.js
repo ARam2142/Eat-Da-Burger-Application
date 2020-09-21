2953b020012df54fc1ab0db5f25571e3a703a758
Aram2142 <andresaaramirez@aol.com>
2020-09-20 22:21:03 -0400

    added delete function

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -20,7 +20,7 @@ $(function() {
           location.reload();
         }
       );
-    });
+    }); 
     
     //enter and submit new burger
     $(".form-create").on("submit", function(event) {
@@ -44,6 +44,22 @@ $(function() {
         }
       );
     });
+
+    $(".trash-can").on("click", function() {
+      var id = $(this).data("id");
+  
+      // Send the DELETE request.
+      $.ajax("/api/burgers/" + id, {
+        type: "DELETE"
+      }).then(
+        function() {
+          console.log("deleted burger", id);
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  
   });
   
 
