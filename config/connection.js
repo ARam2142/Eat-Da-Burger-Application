4c3c57615c4fbfe863c231767bfb57c6e6a96f00
Aram2142 <andresaaramirez@aol.com>
2020-09-21 13:56:16 -0400

    make it better

--- config/connection.js
+++ config/connection.js
@@ -9,7 +9,10 @@ var connection = mysql.createConnection({
 });
 
 connection.connect(function (err) {
-  if (err) throw err;
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
   console.log("connected as id " + connection.threadId + "\n");
 });
 
