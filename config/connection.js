960d5950a10fa6293fa96ed62e3d71bb4b86c8bb
Aram2142 <andresaaramirez@aol.com>
2020-09-21 23:02:23 -0400

    add more things

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,17 @@
-const mysql = require("mysql");
+var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "rootroot",
-  database: "burgers_db"
-});
+if(process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "rootroot",
+    database: "burgers_db"
+  });
 
-var del = connection._protocol._delegateError;
-connection._protocol._delegateError = function(err, sequence){
-  if (err.fatal) {
-    console.trace('fatal error: ' + err.message);
-  }
-  return del.call(this, err, sequence);
-};
+}
 
 connection.connect(function (err) {
   if (err) {
